6485fd354141ed7e869bb279ab60709d0558fbd5
dipespandey <pandey.dipesh50@gmail.com>
2016-11-11 09:36:39 +0545

    modified the trainer

--- recommendations/new.py
+++ recommendations/new.py
@@ -1,10 +1,27 @@
 import numpy as np
+import pandas as pd
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 import random
 
 
-ratings = np.load('recommendations/jhos.npy')
+
+
+# Load data from disk
+def ratings_reader():
+   names = ['user_id', 'item_id', 'rating', 'timestamp']
+   df = pd.read_csv('dataset/u.data', sep='\t', names=names)
+   n_users = df.user_id.unique().shape[0]
+   n_items = df.item_id.unique().shape[0]
+
+   # Create r_{ui}, our ratings matrix
+   ratings = np.zeros((n_users, n_items))
+   for row in df.itertuples():
+       ratings[row[1]-1, row[2]-1] = row[3]
+
+   return ratings
+
+ratings = ratings_reader()
 n_users = ratings.shape[0]
 n_items = ratings.shape[1]
 
@@ -55,33 +72,45 @@ train_rmses = []
 test_rmses = []
 
 
-for n in range(n_iters):
-   for u,i in zip(users, items):
-      prediction = P[u,:].dot(Q[i,:].T) + Bu[u] + Bi[i] + global_bias
-      e = train[u,i] - prediction
-      Bu[u] += gamma * (e - lmbda * Bu[u])
-      Bi[i] += gamma * (e - lmbda * Bi[i])
-      P[u,:] += gamma * (e * Q[i,:] - lmbda * P[u,:])
-      Q[i,:] += gamma * (e * P[u,:] - lmbda * Q[i,:])
-   
-   pred = np.zeros((P.shape[0], Q.shape[0]))
+def trainer(n_iters, users, items, P, Q, Bu, Bi, global_bias, trainer, tester, gamma, lmbda):
+   # use the stochastic gradient descent approach to mimimize the error in prediction
+   train_rmses = []
+   test_rmses = []
 
-   for u in range(P.shape[0]):
-      for i in range(Q.shape[0]):
-         pred[u,i] = P[u,:].dot(Q[i,:].T) + Bu[u] + Bi[i] + global_bias
+   for n in range(n_iters):
+      for u,i in zip(users, items):
+         prediction = P[u,:].dot(Q[i,:].T) + Bu[u] + Bi[i] + global_bias
+         e = train[u,i] - prediction
+         Bu[u] += gamma * (e - lmbda * Bu[u])
+         Bi[i] += gamma * (e - lmbda * Bi[i])
+         P[u,:] += gamma * (e * Q[i,:] - lmbda * P[u,:])
+         Q[i,:] += gamma * (e * P[u,:] - lmbda * Q[i,:])
+      
+      pred = np.zeros((P.shape[0], Q.shape[0]))
 
-    
+      for u in range(P.shape[0]):
+         for i in range(Q.shape[0]):
+            pred[u,i] = P[u,:].dot(Q[i,:].T) + Bu[u] + Bi[i] + global_bias
 
-   train_rmse = get_rmse(train, pred)
-   test_rmse = get_rmse(test, pred)
-   train_rmses.append(train_rmse)
-   test_rmses.append(test_rmse)
-   print(n+1, train_rmse, test_rmse)
+       
+
+      train_rmse = get_rmse(train, pred)
+      test_rmse = get_rmse(test, pred)
+      train_rmses.append(train_rmse)
+      test_rmses.append(test_rmse)
+      print(n+1, train_rmse, test_rmse)
+      return(train_rmses, test_rmses)
+
+
+# add the below line to start the training
+# train_rmses, test_rmses = trainer(n_iters, users, items, P, Q, Bu, Bi, global_bias, train, test, gamma, lmbda)
 
 
 
 
 def draw_learning_curve(n_iters, train_rmse, test_rmse):
+   # use this function to use the above train_rmses and test_rmses to plot the rmse curves, 
+   # see how they converge
    plt.plot(range(n_iters), train_rmse, label = 'Train', linewidth = 5)
    plt.plot(range(n_iters), test_rmse, label = 'Test', linewidth = 5)
    plt.xlabel('Iterations')
